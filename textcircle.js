3c86d8f65442a5e77e9c86160f933470ba739a1a
tanel3203 <tanel3203@gmail.com>
2016-02-04 16:08:34 +0200

    initial

---
+++ textcircle.js
@@ -0,0 +1,36 @@
+this.Documents = new Mongo.Collection("documents");
+
+
+
+if (Meteor.isClient) {
+
+	Template.editor.helpers({
+		docid:function(){
+			return Documents.findOne()._id;
+			var doc=Documents.findOne();
+			if(doc){
+				return doc._id;
+				}
+			else {
+				return "undefined";
+			}
+		},
+		config:function() {
+			return function(editor) {
+				editor.on("change", function(cm_editor, info) {
+					console.log(cm_editor.getValue());
+					$("#viewer_iframe").contents().find('html').html(cm_editor.getValue());
+				});
+			console.log(editor);
+			}
+		},
+	});
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+		if (!Documents.findOne()) {
+			Documents.insert({title:"my new document"});
+		}
+  });
+}
